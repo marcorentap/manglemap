c96477445f64a5a85495eee06634b03e81710daa
Marc Owen <marcowen20@gmail.com>
2024-04-02 18:26:59 +0900

    Fix split() to use newline

--- manglemap.py
+++ manglemap.py
@@ -8,11 +8,11 @@ if len(sys.argv) < 2:
 lib_path = sys.argv[1]
 
 # Get mangled and demangled symbols with nm
-nm_out = subprocess.run(["nm", "--format=just-symbols", lib_path], capture_output=True)
-nm_out2 = subprocess.run(["nm", "--format=just-symbols", "-C", lib_path], capture_output=True)
-mangle_map = list(zip(nm_out.stdout.split(), nm_out2.stdout.split()))
+nm_out = subprocess.run(["nm", "--format=just-symbols", "--numeric-sort", lib_path], capture_output=True)
+nm_out2 = subprocess.run(["nm", "--format=just-symbols", "--numeric-sort", "-C", lib_path], capture_output=True)
+mangle_map = list(zip(nm_out.stdout.split(sep=b'\n'), nm_out2.stdout.split(sep=b'\n')))
 
 for symbol in mangle_map:
 	mangled_name = symbol[0].decode()
 	demangled_name = symbol[1].decode()
-	print(f"{mangled_name} {demangled_name}\n")
+	print(f"{mangled_name} {demangled_name}")
